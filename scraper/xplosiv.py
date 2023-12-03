4271a68f36fb60204583e54479471fe0eda6b937
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2023-12-03 16:03:29 +1300

    new scraper

--- scraper/xplosiv.py
+++ scraper/xplosiv.py
@@ -1,43 +1,55 @@
-import argparse
 import requests
 from bs4 import BeautifulSoup
-import time
+import sys
 
-def scrape(url):
-    # Make an HTTP request to the given URL
-    try:
-        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
-    except requests.exceptions.HTTPError as errh:
-        print ("Http Error:",errh)
-    except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
-    except requests.exceptions.RequestException as err:
-        print ("OOps: Something Else",err)
+def scrape_products(url):
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0'
+    }
+    response = requests.get(url, headers=headers)
+
+    # Only proceed if we got a successful response
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        products = soup.find_all('li', class_='product-item')
+
+        product_data = []
+
+        for product in products:
+            # Find the image URL
+            image_tag = product.find('img', class_='product-image-photo')
+            image_url = image_tag['src'] if image_tag else 'No Image Found'
+
+            # Find the product name
+            name_tag = product.find('a', class_='product-item-link')
+            product_name = name_tag.text.strip() if name_tag else 'No Name Found'
+            product_link = name_tag['href'] if name_tag and name_tag.has_attr('href') else 'No Link Found'
+
+            # Find the lowest price
+            price_tag = product.find('span', class_='price')
+            price = price_tag.text if price_tag else 'No Price Found'
+
+            product_data.append({
+                'product_name': product_name,
+                'price': price,
+                'image_url': image_url,
+                'link': product_link
+            })
+
+        return product_data
     else:
-        # Parse the content of the page
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup
-
-def main(url):
-    # Use the provided URL to scrape
-    soup = scrape(url)
-    price_element = soup.find(class_='price-wrapper')
-    product_element = soup.find('span', class_='base')
-    if price_element and product_element:
-        # Extract the data-price-amount attribute
-        price_amount = price_element.get('data-price-amount')
-        product_name = product_element.text
-        print(product_name, price_amount)
-
-# Set up argparse
-parser = argparse.ArgumentParser(description='A simple CLI web scraper.')
-parser.add_argument('url', type=str, help='The URL to scrape')
-args = parser.parse_args()
+        print("Failed to retrieve the webpage")
+        return []
 
 if __name__ == "__main__":
-    # Implement a simple delay to respect the website's server by not overwhelming it with requests
-    time.sleep(1) # Sleep for 1 second before starting the scrape to avoid hitting the server too hard
-    main(args.url)
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    products = scrape_products(url)
+
+    for product in products:
+        print(product)
