428ec5f5e06029df3551411516836c8bcec5b20c
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-09 22:36:20 +1300

    logo hover

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,12 +6,12 @@ function Header () {
         <>
         <header className='header'>
             <nav>
-            <ul className='nav-left'>
-                <li><a>Home</a></li>
-                <li><a>Products</a></li>
-                <li><a>Admin</a></li>
-            </ul>
-            <h1 className='logo'>Supp$aver</h1>
+                <ul className='nav-left'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Admin</a></li>
+                </ul>
+            <h1 className='logo' href="#">Supp$aver</h1>
             <div className='nav-right'>
                 <img className='user' src={userLogo} alt="User Logo" />
             </div>
