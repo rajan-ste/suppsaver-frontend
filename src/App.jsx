5176e2a1ef36f0f70c6bef912cfc531a45fe2e1d
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-10 16:32:36 +1300

    finish homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import HomePage from './HomePage';
 import Header from './components/header/header'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <Header />
-      </div>
-    </>
-  )
+    <BrowserRouter >
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
 export default App
