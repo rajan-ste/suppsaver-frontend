168bf822365339a89f89e492737e61223ced41cf
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-11 15:28:51 +1300

    product endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
+import Products from './Products';
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import './App.css'
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
       <Footer />
     </Router>
