3ed18f04640d50c9251c5b48d8ea0323cddbb7e7
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-11 13:14:58 +1300

    footer and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter >
+    <Router >
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
+      <Footer />
+    </Router>
   );
 }
 
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,6 @@
 import './Homepage.css';
 import trainer from './assets/trainer.svg';
+import { Link } from "react-router-dom";
 
 function HomePage () {
     return (
@@ -8,7 +9,7 @@ function HomePage () {
                 <div className='top-box'>
                     <h1>SuppSaver</h1>
                     <p>Find the best deal on your fitness supplements by comparing prices across three of New Zealand&apos;s largest supplement retailers.</p>
-                    <a className='explore' href='#'>Explore &#8594;</a>
+                    <Link to="products" className='explore'>Explore &#8594;</Link>
                 </div>
                 <div className='middle-box'>
                     <img src={trainer}></img>
