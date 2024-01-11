09481a98637b31c0eaa0c1329da5fed2596c628d
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-11 13:14:58 +1300

    footer and links

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import './header.css'
 import userLogo from './../../assets/user.svg'
+import { Link } from "react-router-dom";
 
 function Header () {
     return (
@@ -7,18 +8,20 @@ function Header () {
         <header className='header'>
             <nav>
                 <ul className='nav-left'>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#">Admin</a></li>
+                    <li><Link to="/">Home</Link></li> 
+                    <li><Link to="/products">Products</Link></li> 
+                    <li><Link to="/admin">Admin</Link></li> 
                 </ul>
-            <h1 className='logo' href="#">Supp$aver</h1>
-            <div className='nav-right'>
-                <img className='user' src={userLogo} alt="User Logo" />
-            </div>
+                <Link to="/" className='logo'>Supp$aver</Link> 
+                <div className='nav-right'>
+                    <div className='user-wrapper'>
+                        <Link to="/login"><img className='user' src={userLogo} alt="User Logo" /></Link>
+                    </div>
+                </div>
             </nav>
         </header>
         </>
     );
 }
 
-export default Header
+export default Header;
