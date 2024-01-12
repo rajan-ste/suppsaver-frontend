3b83be5cb7baffd46eaca559a2064a49d3514d2c
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-12 16:29:27 +1300

    product-company fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import Products from './Products';
+import Product from './Product';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </div>
       <Footer />
