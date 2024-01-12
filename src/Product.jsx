649c38b33c84cf9af4e54ba6a3c5412cc32987cc
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-12 21:40:25 +1300

    working prod links and logos

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import './Product.css';
 
-function ProductList() {
+function Product() {
     const [products, setProducts] = useState([]);
     let { id } = useParams();
 
@@ -21,21 +22,46 @@ function ProductList() {
 
     return (
         <>
-        <div className="page-title"><h1>Products</h1></div>
-        <div><h1>search bar here</h1></div>
         <div className='wrapper'>
             {products.map((product, i) => {
-                return <div to={"/products/" + product.id} className={product.price === 0 ? 'product-zero' : 'product'} key={i}>
+                return <Link to={product.link} target="_blank" className={product.price === 0 ? 'product-zero' : 'product'} key={i}>
                             <figure>
+                                {<Company companyid={product.companyid}/>}
                                 <img className='product-image' src={product.image} alt={product.product_name}></img>
                                 <figcaption>{product.product_name}</figcaption>
                                 <figcaption className='prod-price'>{product.price === 0 ? "Product Unavailable" : `$${product.price.toFixed(2)}`}</figcaption>
                             </figure>    
-                        </div>
+                        </Link>
                         })}
                     </div>        
         </>
     ); 
 }
 
-export default ProductList;
+
+function Company( { companyid }) {
+    const [companies, setCompanies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/companies')
+            .then(response => response.json())
+            .then(data => {
+                const filteredCompanies = data.filter(company => company.id === companyid);
+                setCompanies(filteredCompanies);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, [companyid]);
+
+    return (
+        <>
+            {companies.map((company, i) => (
+                <div key={i}>
+                    <img src={company.logo} alt={company.name} className={companyid === 3 ? 'sprintfit-logo' : 'logo-other'}/>
+                </div>
+            ))}
+        </>
+    );
+}
+
+
+export default Product;
