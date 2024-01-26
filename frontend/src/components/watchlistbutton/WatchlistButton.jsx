f15b159bb4ddcb9e20912091f0983fee56dda2e8
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-26 21:32:09 +1300

    watchlist button css

---
+++ frontend/src/components/watchlistbutton/WatchlistButton.jsx
@@ -0,0 +1,12 @@
+import './WatchlistButton.css'
+
+function WatchlistButton () {
+
+    return (
+        <>
+            <button className="watchlist-button hvr-float">Add To Watchlist</button>
+        </>
+    )
+}
+
+export default WatchlistButton
