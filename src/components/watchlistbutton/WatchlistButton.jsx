283c7f887115ac6ae7ab5be1ded9446ebd4198e6
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-27 20:06:17 +1300

    fully functional watchlist button on products

--- src/components/watchlistbutton/WatchlistButton.jsx
+++ src/components/watchlistbutton/WatchlistButton.jsx
@@ -7,36 +7,48 @@ import { useState } from 'react';
 function WatchlistButton () {
 
     let { productid } = useParams();
-
-    // true if product is not in watchlist, else false
     
-
     const setWatchlistState = async () => {
         try {
             const response = await axios.get('http://localhost:8080/api/watchlist');
             const watchlistData = response.data;
-            const filteredData = watchlistData.filter(product => product.productid == productid)
-            filteredData.length > 0 && setAdd(false)
-            console.log(filteredData);
+            const isProductInWatchlist = watchlistData.some(product => product.productid == productid);
+            setAdd(!isProductInWatchlist); 
         } catch (error) {
             console.error('Error fetching watchlist data: ', error);
         }
-    }
+    };
     
+    
+    // true if product is not in watchlist, else false
     const [add, setAdd] = useState(setWatchlistState);
 
     const handleWatchlistAdd = async () => {
         try {
             const response = await axios.post('http://localhost:8080/api/watchlist', { userid: null, productid: productid});
+            setWatchlistState();
             console.log(response)
         } catch (error) {
             console.error('Watchlist add failed:', error);
         }
     }
 
+    const handleWatchlistDelete = async () => {
+        try {
+            const response = await axios.delete('http://localhost:8080/api/watchlist', {
+                data: { userid: null, productid: productid } 
+            });
+            await setWatchlistState();
+            console.log(response)
+        } catch (error) {
+            console.error('Watchlist delete failed:', error);
+        }
+    }
+    
     return (
         <>
-            <button onClick={handleWatchlistAdd} className="watchlist-button">{add ? "Add to watchlist" : "Remove from watchlist"}</button>
+            <button onClick={add ? handleWatchlistAdd : handleWatchlistDelete} className={add ? "watchlist-button-add" : "watchlist-button-delete"}>
+                {add ? "Add to watchlist" : "Remove from watchlist"}</button>
         </>
     )
 }
