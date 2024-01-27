10e8728309edc3d6015ae40a93f99f88408adf30
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-27 14:08:30 +1300

    add to watchlist button server interactions

--- src/components/watchlistbutton/WatchlistButton.jsx
+++ src/components/watchlistbutton/WatchlistButton.jsx
@@ -1,10 +1,42 @@
 import './WatchlistButton.css'
+import axios from '../../api/api';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+
 
 function WatchlistButton () {
 
+    let { productid } = useParams();
+
+    // true if product is not in watchlist, else false
+    
+
+    const setWatchlistState = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/watchlist');
+            const watchlistData = response.data;
+            const filteredData = watchlistData.filter(product => product.productid == productid)
+            filteredData.length > 0 && setAdd(false)
+            console.log(filteredData);
+        } catch (error) {
+            console.error('Error fetching watchlist data: ', error);
+        }
+    }
+    
+    const [add, setAdd] = useState(setWatchlistState);
+
+    const handleWatchlistAdd = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/api/watchlist', { userid: null, productid: productid});
+            console.log(response)
+        } catch (error) {
+            console.error('Watchlist add failed:', error);
+        }
+    }
+
     return (
         <>
-            <button className="watchlist-button hvr-float">Add To Watchlist</button>
+            <button onClick={handleWatchlistAdd} className="watchlist-button">{add ? "Add to watchlist" : "Remove from watchlist"}</button>
         </>
     )
 }
