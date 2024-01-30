a7e0874adc48830896b2c9d885939dafea07dda6
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-30 16:32:56 +1300

    fork db

--- api/src/app/models/db.js
+++ api/src/app/models/db.js
@@ -5,10 +5,10 @@ const dbUrl = process.env.CLEARDB_DATABASE_URL;
 const dbParts = dbUrl.match(/mysql:\/\/(.*):(.*)@(.*):(.*)\/(.*)\?/);
 
 const connection = mysql.createConnection({
-  host: dbParts[3],
-  user: dbParts[1],
-  password: dbParts[2],
-  database: dbParts[5]
+  host: "us-cluster-east-01.k8s.cleardb.net",
+  user: "b27037be334874",
+  password: "1cfb8b43",
+  database: "heroku_6d10f5d88d3405d"
 });
 
 connection.connect(error => {
