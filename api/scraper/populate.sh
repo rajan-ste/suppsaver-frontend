9e43f5f55e2c8137a09bed1479594eec73f0a118
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-02-14 11:55:13 +1300

    .sh file again

--- api/scraper/populate.sh
+++ api/scraper/populate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=2
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=3
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=4
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/moderate-stimulant.html
-python scraper/xplosiv.py https://xplosiv.nz/pre-workout/moderate-stimulant.html?p=2
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=2
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=3
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/extreme-stimulant.html?p=4
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/moderate-stimulant.html
+python /app/scraper/xplosiv.py https://xplosiv.nz/pre-workout/moderate-stimulant.html?p=2
 
-python scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout
-python scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=2
-python scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=3
-python scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=4
-python scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=5
+python /app/scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout
+python /app/scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=2
+python /app/scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=3
+python /app/scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=4
+python /app/scraper/suppsnz.py https://www.supplements.co.nz/collections/pre-workout?page=5
 
-python scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout
-python scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout?pgNmbr=2
-python scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout?pgNmbr=3
+python /app/scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout
+python /app/scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout?pgNmbr=2
+python /app/scraper/sprintfit.py https://www.sprintfit.co.nz/products/category/316/pre-workout?pgNmbr=3
